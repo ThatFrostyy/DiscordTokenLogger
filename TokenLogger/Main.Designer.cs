0500e6db7adfcce637b33b42bf9933b962f9b725
Administrator <Administrator@NEXUSLITE-PC>
2024-07-10 15:53:30 +0200

    Add project files.

---
+++ TokenLogger/Main.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TokenLogger
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Discord Token";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(530, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(530, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webView
+            // 
+            this.webView.ActivateBrowserOnCreation = false;
+            this.webView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(524, 422);
+            this.webView.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Image = ((System.Drawing.Image)(resources.GetObject("checkBox1.Image")));
+            this.checkBox1.Location = new System.Drawing.Point(698, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(39, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 422);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Discord Token Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        public CefSharp.WinForms.ChromiumWebBrowser webView;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ TokenLogger/Main.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Forms;
+using CefSharp;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TokenLogger
+{
+    public partial class Main : Form
+    {
+        private string token;
+        private System.Timers.Timer timer;
+
+        public Main()
+        {
+            InitializeComponent();
+            webView.LoadUrl("https://discord.com/login");
+
+            Delay();
+
+            timer = new System.Timers.Timer(5000); 
+            timer.Elapsed += new ElapsedEventHandler(ToggleComps);
+            timer.Start();
+        }
+
+        private async void Delay()
+        {
+            await Task.Delay(5000);
+
+            if (webView.Address == "https://discord.com/login")
+            {
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                checkBox1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show($"Already logged in, skiping login.", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ToggleComps(object source, ElapsedEventArgs e)
+        {
+            if (!this.IsDisposed)
+            {
+                this.Invoke((Action)delegate
+                {
+                    if (webView.Address == "https://discord.com/login")
+                    {
+                        button1.Enabled = true;
+                        textBox1.Enabled = true;
+                        checkBox1.Enabled = true;
+                    }
+                    else
+                    {
+                        button1.Enabled = false;
+                        textBox1.Enabled = false;
+                        checkBox1.Enabled = false;
+                    }
+                });
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if (webView.Address == "https://discord.com/login")
+            {
+                var script = File.ReadAllText("script.js");
+                script = script.Replace("TOKEN", token);
+
+                try
+                {
+                    webView.ExecuteScriptAsync(script);
+                    webView.Refresh();
+
+                    button1.Enabled = false;
+                    textBox1.Enabled = false;
+                    checkBox1.Enabled = false;
+
+                    MessageBox.Show($"Successfully logged in using the token.", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"There was an error while executing the script: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }           
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            token = textBox1.Text;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            timer.Stop();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                timer.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
